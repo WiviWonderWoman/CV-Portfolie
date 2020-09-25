028d6cd6efa19cafea8045ad3708843d13f21d50
Wivianne Grapenholt <w.grapenholt@gmail.com>
2020-09-25 08:38:16 +0200

    testar git länk

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+'use strict';
+let arrGit = [];
+
 fetch('https://api.github.com/users/WiviWonderWoman/repos')
 .then(resp => resp.json())
-.then(data => console.log(data))
+.then(data => gitHub(data))
+
+
+function gitHub(data) {
+    (data);
+    arrGit = data;
+    console.log(arrGit);
+    arrGit.forEach(element => {
+        // document.getElementById('github').insertAdjacentHTML ("afterbegin", `<button id="knapp"></button>`);
+        document.getElementById('github').insertAdjacentHTML("afterbegin",`<a href=`+element.html_url+`target="_blank">`+element.name+`</a>`)
+        
+    }); 
+}  
+
+
+ // console.log(element.html_url);
+ // console.log(element.name);       
+
